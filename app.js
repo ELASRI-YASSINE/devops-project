8e573b36bc92c480019795a93153e70a488fcb55
yassine <elasriyassine000@gmail.com>
2025-10-20 10:06:48 +0100

    initial commit - dockerized node.js app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const Port = 3000;
+app.get('/', (req, res)=>{
+res.send("hello from docker");
+});
+app.get('/tasks', (req, res)=>{
+res.json([
+{id:1, name: "on dockerise cette app"},
+{id:2, name: "on utilise githup actions pour ci/cd"}
+]);
+});
+app.listen(Port, ()=>{
+console.log("app ecoute dans le port 3000");
+});
